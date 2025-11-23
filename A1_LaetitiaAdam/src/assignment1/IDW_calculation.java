b1c9b3a0a602089fb27f1a4428b2ac0a34020220
Laetitia <laetitia.adam@htp-tel.de>
2025-11-23 15:05:02 +0100

    for-loop iterating through 2D array

--- A1_LaetitiaAdam/src/assignment1/IDW_calculation.java
+++ A1_LaetitiaAdam/src/assignment1/IDW_calculation.java
@@ -1,5 +1,40 @@
+
+
 package assignment1;
 
-public class IDW_calculation {
+import java.util.Arrays;
 
+public class IDW_calculation {
+	
+	
+	
+public static double[][] idw(double[][] data) {
+	
+	int row = data.length;
+	int col = data[0].length;
+	
+	double [][] result = new double [row][col];
+	
+	for (int rowNumber = 0; rowNumber < row; rowNumber++) {
+		for (int colNumber = 0; colNumber < col; colNumber++) {
+			result[rowNumber][colNumber] = data[rowNumber][colNumber];
+		}
+	}
+	
+	
+	return result;
 }
+
+public static void main (String[] args) {
+	
+	double[][] grid = {
+			{1.0, Double.NaN, 3.0},
+			{Double.NaN, Double.NaN, 2.0},
+			{4.0, 5.0, Double.NaN}
+	};
+	
+	double[][] interpolatedGrid = idw(grid);
+	System.out.println(Arrays.deepToString(interpolatedGrid));
+	}
+}
+
